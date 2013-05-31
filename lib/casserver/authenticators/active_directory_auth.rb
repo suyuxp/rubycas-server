61489d12e3de714348977be6ddf2827d9dceb0ce
suyuxp <suyu@zhiyisoft.com>
2013-05-31 20:12:03 +0800

    Custom Active Directory Authenticator

--- lib/casserver/authenticators/active_directory_auth.rb
+++ lib/casserver/authenticators/active_directory_auth.rb
@@ -19,17 +19,15 @@ class CASServer::Authenticators::ActiveDirectoryAuth < CASServer::Authenticators
     raise CASServer::AuthenticatorError, "Invalid E-Mail authenticator configuration!" unless @options[:ad]
     raise CASServer::AuthenticatorError, "You must specify a Doamin, a Server and a Base in the ActiveDirectoryAuth configuration!" unless (@options[:ad][:domain] && @options[:ad][:server] && @options[:ad][:base])
 
-    Adauth.configure do |c|
-      c.domain = @options[:ad][:domain]
-      c.server = @options[:ad][:server]
-      c.base   = @options[:ad][:base]
-    end
-
     begin
-      auth = Adauth.authenticate(@username, @password)
-      return auth.status == "235" if auth
+      Adauth.configure do |c|
+        c.domain = @options[:ad][:domain]
+        c.server = @options[:ad][:server]
+        c.base   = @options[:ad][:base]
+      end
 
-      throw Exception
+      auth = Adauth.authenticate(@username, @password)
+      return not auth.nil?
     rescue => e
       raise CASServer::AuthenticatorError,
         "Active Directory authentication failed with '#{e}'. Check your authenticator configuration."
